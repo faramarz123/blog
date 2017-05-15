fea7459bc8275475a773bf234cb0a1ed4c334345
faramarz <faramarz.razmi@gmail.com>
2017-05-16 02:05:06 +0430

    Seting authentication thru cookie

--- blog.js
+++ blog.js
@@ -1,46 +1,51 @@
-$(document).ready(function()
-{
+$(document).ready(function () {
 
-        //pageLoad();
-        //$("submit").
-        authentication();
+    //pageLoad();
+    //$("submit").
+    authentication();
 
 
-    function pageLoad()
-    {
+    function pageLoad() {
         // authentication();
     }
 
-    function authentication()
-    {
+    function authentication() {
         //$("submit")
         console.log("hello");
 
         //parameters
 
 
-        $('#submit').click(function(e)
-        {
-             console.log("hello from click2");
+        $('#loginform').submit(function (e) {
             e.preventDefault();
+            console.log("hello from click2");
+            // e.preventDefault();
+
             //var form = $(this);
-        var username = $('#username').val();
-        var password = $('#password').val(); 
-        var loginData = {username: username,password: password};
+            var username = $('#username').val();
+            var password = $('#password').val();
+            var loginData = { username: username, password: password };
+            //      $.post("https://ancient-bayou-43826.herokuapp.com/login",
+            // {
+            //     username: username,
+            //     password: password
+            // },
+            // function(data){
+            //     alert("Data: " + data.token  );
+            // });
             $.ajax({
                 url: 'https://ancient-bayou-43826.herokuapp.com/login',
                 type: 'post',
                 contentType: 'application/json',
                 dataType: 'json',
-                async: true,
-                data: JSON.stringify(loginData) ,
-                success: function()
-                {
+                data: JSON.stringify(loginData),
+                success: function () {
                     console.log('Hello from success function. ');
+                    $.cookie('username', 'pgtest', { expires: 1 });
+                    var currentusr = $.cookie('username');
                     //var token = result.token;
-                },   
-                error: function(xhr,textStatus,errorThrown)
-                {
+                },
+                error: function (xhr, textStatus, errorThrown) {
                     console.log('Hello from erroe function. ');
                     // console.log('error');
                 }
