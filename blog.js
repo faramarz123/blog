98be9bda57295e840ed3cf68abdff5e693f0cfa7
faramarz <faramarz.razmi@gmail.com>
2017-05-17 14:01:28 +0430

    Append posts to posts.html

--- blog.js
+++ blog.js
@@ -2,9 +2,8 @@ $(document).ready(function () {
 
     //PageLoad function()
     pageLoad();
-    if(eraseCookie('token1')) 
-    {
-        console.log("token1 is erased. ");  
+    if (eraseCookie('token1')) {
+        console.log("token1 is erased. ");
     }
 });
 
@@ -12,15 +11,13 @@ $(document).ready(function () {
 function pageLoad() {
     // eraseCookie('token');
     //Is there a authenticated user or not
-    if (readCookie('token')) 
-    {
-        $('.postsPanel').show();
+    if (readCookie('token')) {
+        $('.posts-panel').show();
         // showDOMElement("#posts");
         loadPosts();
     }
-    else 
-    {
-        $('#loginPanel').show();
+    else {
+        $('#login-panel').show();
         // showDOMElement("#loginform");
         //Login function to handle form submit and set cookie, then, call LoadPosts().
         // login();
@@ -31,7 +28,7 @@ function pageLoad() {
 function login() {
 
     //Login on form submit 
-    $('#loginform').submit(function (e) {
+    $('#login-form').submit(function (e) {
         e.preventDefault();
         console.log("hello from login submit");
 
@@ -39,7 +36,7 @@ function login() {
         var username = $('#username').val();
         var password = $('#password').val();
         var loginData = { username: username, password: password };
-        createCookie('token1', 'laskdj', 1);
+
 
         //HttpPostRequest to post login info.
         $.ajax({
@@ -53,16 +50,12 @@ function login() {
                 // getPostsToken = data.token;
 
                 //Set the got toke to cookie
-                createCookie('token', data.token, 1);
+                createCookie('token', data.token, 10);
                 // readCookie('token');
                 // eraseCookie('token');
-                // eraseCookie('token1');
-
-                //Hide loginDOM before showing the posts
-                hideDOMElement("#loginform");
 
                 //Call loadPosts function to load posts
-                // loadPosts();
+                loadPosts();
                 // console.log("This is loadPosts(). ");
 
             },
@@ -102,13 +95,30 @@ function loadPosts() {
         headers: {
             'token': readCookie('token')
         },
-        success: function (posts) {
+        success: function (Posts) {
+
             console.log("This is load post success. ");
-            // console.log(posts);
-            // $.each(data, function (index, post) {
-            //     $("#posts").append("<li><span>" + post.text + "</span> <button type='button' value='load comments' onclick='loadcomments(" + post.id + ")' /></li>");
-            //     // $("#ul").append('<li>post: ' + post + '</li>');
-            // });
+            // var posts1 = posts.split("");
+            console.log(Posts.posts, " error is :", Posts.error);
+            console.log("Posts are loaded. ");
+            var postsDOM = $(".posts");
+            //var myposts = [];
+            var myposts = Posts.posts;
+            console.log(myposts[0].content);
+
+            // console.log("Myposts length is : ", myposts.length);
+            myposts.forEach(function (post) {
+                var li = document.createElement("LI");
+                
+                var liText = document.createTextNode("پست شماره : "+" ( "+myposts.indexOf(post)+" ) "+ post.content);
+                li.appendChild(liText);
+                postsDOM.append(li);
+
+                // postsDOM.append("<li><span>" + post.content + "</span> <button type='button' value='load comments' onclick='loadcomments(" + post.id + ")' /></li>");
+                
+            });
+            console.log("posts are appended to DOM. ");
+            
         }
     });
 }
